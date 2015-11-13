e3b9ffd40b8d29545ddb7c4492d27f7f1345d305
Pavel Cvrček <jasnapaka@jasnapaka.com>
2015-11-13 10:38:46 +0100

    Aktualizace šablony

---
+++ index-content.php
@@ -0,0 +1,10 @@
+<h2>Snímek Stopovaná</h2>
+
+<p id="titulka-mapa">Krátký dokument stopuje šipky namalované na starých fasádách plzeňských domů. 
+    Dokument odpovídá krátkou výpovědí několika kolemjdoucích na otázky po jejich původu a účelu. 
+    Někteří nevěří, že by šipky na fasádách byly něčím víc než pouhým ukazatelem, jiní rozpomínají na dobu vzniku těchto šipek.</p><br />
+
+<iframe src="https://player.vimeo.com/video/139959214?title=0&byline=0&portrait=0" 
+        width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+
+
